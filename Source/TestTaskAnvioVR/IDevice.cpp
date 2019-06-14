5732e58ab3fbfbf8e8424c5cf217b61012ba5451
Mikhail S. Kataev <lpestlname@gmail.com>
2019-06-14 22:58:16 +0300

    Added ThingBase class

---
+++ Source/TestTaskAnvioVR/IDevice.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IDevice.h"
+
+// Add default functionality here for any IIDevice functions that are not pure virtual.
---
+++ Source/TestTaskAnvioVR/ThingBase.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThingBase.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+AThingBase::AThingBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ThingMeshComponent"));
+	VisualMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+}
+
+// Called when the game starts or when spawned
+void AThingBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AThingBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
